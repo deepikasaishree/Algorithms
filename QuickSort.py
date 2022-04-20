4061309c148e2fcec52f27b593f3db28c6f3f2b5
Deepika <shree0417@gmail.com>
2022-04-20 21:14:02 +0530

    QuickSort added

---
+++ QuickSort.py
@@ -0,0 +1,18 @@
+def quickSort(arr):
+    if len(arr)>1:
+        pivot=arr.pop()
+        greaterList,equalList,smallerList=[],[pivot],[]
+        for i in arr:
+            if i==pivot:
+                equalList.append(i)
+            elif i>pivot:
+                greaterList.append(i)
+            else:
+                smallerList.append(i)
+        return quickSort(smallerList)+equalList+quickSort(greaterList)
+    else:
+        return arr
+
+if __name__=="__main__":
+    arr=[24,21,22,32,40]
+    print(quickSort(arr))
