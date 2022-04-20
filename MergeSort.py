82d54f63d8cbeb896f961aa56d529593d42cacef
Deepika <shree0417@gmail.com>
2022-04-20 21:00:57 +0530

    MergeSort added

---
+++ MergeSort.py
@@ -0,0 +1,35 @@
+def mergeSort(arr):
+    if len(arr)==1:
+        return arr
+    m=(len(arr)-1)//2
+    l1=mergeSort(arr[:m+1])
+    l2=mergeSort(arr[m+1:])
+    res=merge(l1,l2)
+    return res
+
+def merge(l1,l2):
+    i=0
+    j=0
+    l=[]
+    while i<=len(l1)-1 and j<=len(l2)-1:
+        if l1[i]<l2[j]:
+            l.append(l1[i])
+            i+=1
+        else:
+            l.append(l2[j])
+            j+=1
+    if i>len(l1)-1:
+        while j<=len(l2)-1:
+            l.append(l2[j])
+            j+=1
+    else:
+        while i<=len(l1)-1:
+            l.append(l1[i])
+            i+=1
+    return l
+
+
+
+arr=[9,20,1,24,7]
+print(mergeSort(arr))
+    
